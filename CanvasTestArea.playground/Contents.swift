064a4f970e1a26312e03d7056f356b3fcce58c79
Chiagozie, Zachary <zchiagozie@lcs-mba3000.lcs.on.ca>
2017-10-04 11:51:16 -0400

    Got My First Ear

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -21,18 +21,17 @@ import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
-
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
+let canvas = Canvas(width: 500, height: 500)
 
 // Show where the origin is
 canvas.drawAxes()
 
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+// Draw the ears
+canvas.drawEllipse(centreX: 150, centreY: 300, width: 100, height: 50)
+
 
 
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
+
+
