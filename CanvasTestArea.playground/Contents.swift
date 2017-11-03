dfc063a582d57bfd67fe451b03f42f42631587f8
Chiagozie, Zachary <zchiagozie@lcs-mba3000.lcs.on.ca>
2017-11-03 11:16:00 -0400

    Ya boy flexing hard. UH  this is the latest panda model ya dig

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -32,10 +32,24 @@ canvas.fillColor = Color.white
 canvas.drawEllipse(centreX: 250, centreY: 250, width: 245, height: 240)
 canvas.fillColor = Color.black
 canvas.rotate(by: 20)
-canvas.drawEllipse(centreX: 296, centreY: 267, width: 75, height: 80)
+canvas.drawEllipse(centreX: 296, centreY: 290, width: 75, height: 80)
 canvas.rotate(by: 340)
-canvas.drawEllipse(centreX: 318, centreY: 348, width: 75, height: 80)
-canvas.drawEllipse(centreX: 250, centreY: 250, width: 50, height: 50)
+canvas.drawEllipse(centreX: 318, centreY: 375, width: 75, height: 80)
+canvas.drawEllipse(centreX: 250, centreY: 220, width: 20, height: 30)
+canvas.drawEllipse(centreX: 318, centreY: 265, width: 20, height: 30)
+canvas.drawEllipse(centreX: 190, centreY: 265, width: 20, height: 30)
+canvas.fillColor = Color.white
+canvas.drawShapesWithBorders
+canvas.drawEllipse(centreX: 190, centreY: 265, width: 10, height: 10)
+canvas.drawEllipse(centreX: 318, centreY: 265, width: 10, height: 10)
+
+canvas.fillColor = Color.init(hue: 332, saturation: 100, brightness: 78, alpha: 50)
+canvas.drawEllipse(centreX: 190, centreY: 220, width: 20, height: 20)
+canvas.drawEllipse(centreX: 318, centreY: 220, width: 20, height: 20)
+
+canvas.drawShapesWithBorders
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 250, centreY: 180, width: 30, height: 10)
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
 
